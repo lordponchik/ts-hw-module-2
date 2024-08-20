cc788015e230c5220abd31f62bfe9611125c15e5
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-08-20 19:15:34 +0200

    add tasks 1-4

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,56 @@
+// task-01
+
+function getPromise(): Promise<[string, number]> {
+  return new Promise((resolve) => {
+    resolve(["Text", 50]);
+  });
+}
+
+getPromise().then((data) => {
+  console.log(data);
+});
+
+// task-02
+
+type AllType = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number;
+};
+
+function compare(
+  top: Pick<AllType, "name" | "color">,
+  bottom: Pick<AllType, "position" | "weight">
+): AllType {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  };
+}
+
+// task-03
+
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
+  return Object.assign(objA, objB);
+}
+
+// task-04
+
+class Component<T> {
+  constructor(public props: T) {}
+}
+
+interface PageProps {
+  title: string;
+}
+
+class Page extends Component<PageProps> {
+  pageInfo() {
+    console.log(this.props.title);
+  }
+}
+
 export {};
